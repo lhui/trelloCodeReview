70889f1a96885d2c73e21440495ea8490ec3b040
GitHub Action <action@github.com>
2021-06-06 14:43:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/tcpgnl/trellocodereview/MyBundle.kt
+++ src/main/kotlin/com/github/tcpgnl/trellocodereview/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.tcpgnl.trellocodereview
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/tcpgnl/trellocodereview/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tcpgnl/trellocodereview/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tcpgnl.trellocodereview.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tcpgnl.trellocodereview.MyBundle
 
 class MyApplicationService {
 
